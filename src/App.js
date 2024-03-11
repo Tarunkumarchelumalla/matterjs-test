605fe585df819e71ac322fc649caaae67111a56a
Tarunkumarchelumalla <tarunchelumalla@gmail.com>
2024-03-12 01:20:23 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 
 import { useEffect, useRef } from 'react'
-import { Engine, Render, Bodies, World ,Runner, Constraint,} from 'matter-js'
+import { Engine, Render, Bodies, World, Runner, Mouse, MouseConstraint, Composite } from 'matter-js'
 
-function App (props) {
+function App(props) {
   const scene = useRef()
   const isPressed = useRef(false)
   const engine = useRef(Engine.create())
-  const engine2 = useRef(Engine.create())
 
-  engine.current.gravity ={
-    x:0,
-    y:-5,
-    scale:0
+  engine.current.gravity = {
+    x: 0,
+    y: -5,
+    scale: 0
   }
 
   engine.current.velocityIterations = 4
   engine.current.positionIterations = 6
   engine.current.constraintIterations = 2
 
-  let count =0;
+  let count = 0;
+
   useEffect(() => {
     const cw = document.body.clientWidth
     const ch = document.body.clientHeight
@@ -35,21 +35,33 @@ function App (props) {
     })
 
     World.add(engine.current.world, [
-      Bodies.rectangle(cw / 2, -10, cw, 20, { isStatic: true,restitution:1.2 }),
-      Bodies.rectangle(-10, ch / 2, 20, ch, { isStatic: true,restitution:1.2 }),
-      Bodies.rectangle(cw / 2, ch + 10, cw, 20, { isStatic: true,restitution:1.2 }),
-      Bodies.rectangle(cw + 10, ch / 2, 20, ch, { isStatic: true,restitution:1.2 })
+      Bodies.rectangle(cw / 2, -10, cw, 20, { isStatic: true, restitution: 1.2 }),
+      Bodies.rectangle(-10, ch / 2, 20, ch, { isStatic: true, restitution: 1.2 }),
+      Bodies.rectangle(cw / 2, ch + 10, cw, 20, { isStatic: true, restitution: 1.2 }),
+      Bodies.rectangle(cw + 10, ch / 2, 20, ch, { isStatic: true, restitution: 1.2 })
     ])
 
-    const blackHole = Bodies.circle(600,300,40,{
-      density:500,
-      isStatic:true,
-      render:{
-        fillStyle:'red'
-      }
-    })
+    var mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine.current, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false
+          }
+        }
+      });
+    Composite.add(engine.current.world, mouseConstraint);
 
-    World.add(engine.current.world,[blackHole])
+    // keep the mouse in sync with rendering
+    render.mouse = mouse;
+
+    // fit the render viewport to the scene
+    Render.lookAt(render, {
+      min: { x: 0, y: 0 },
+      max: { x: cw, y: ch }
+    });
+
     Runner.run(engine.current)
     Render.run(render)
 
@@ -74,31 +86,32 @@ function App (props) {
 
   const handleAddCircle = e => {
     count++;
-    if(count > 40) return
+    if (count > 40) return
     console.log(isPressed)
     if (!isPressed.current) {
       const ball = Bodies.circle(
         e.clientX,
         e.clientY,
         10 + Math.random() * 30,
-        
+
         {
           mass: 10,
-          frictionAir:0.01,
-          density:0.001,
-          frictionStatic:0.5,
+          frictionAir: 0.01,
+          density: 0.001,
+          frictionStatic: 0.5,
           restitution: 1.2,
           friction: 0.1,
-          force:{
-            x:0.5,
-            y:0.6
+          id: `id-${count}`,
+          force: {
+            x: 0.5,
+            y: 0.6
           },
           render: {
             fillStyle: '#000'
           }
         })
       World.add(engine.current.world, [ball])
-    }    
+    }
   }
 
   return (
@@ -113,7 +126,6 @@ function App (props) {
 }
 
 export default App;
-
 
 // import * as React from "react";
 // import {
